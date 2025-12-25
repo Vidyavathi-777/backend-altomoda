f203d8da2218da23590344d87fcbb9d17cd88069
Vidyavthi-777 <vidyavathidigadari_jtbb2@jtdfpundation.org>
2025-12-25 13:24:17 +0530

    401

--- backend/src/app.js
+++ backend/src/app.js
@@ -42,6 +42,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
 }));
 
 app.options("*", cors());
